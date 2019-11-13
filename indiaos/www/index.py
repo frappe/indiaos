df0d4790487e83f91929b7bd707e21b151277985
Shivam Mishra <scm.mymail@gmail.com>
2019-11-13 11:05:24 +0530

    fix: permission issue

--- indiaos/www/index.py
+++ indiaos/www/index.py
@@ -1,4 +1,4 @@
 import frappe
 
 def get_context(context):
-	context.speakers = frappe.get_list("Conference Speakers", filters={'published': 1}, fields=['photo', 'full_name', 'designation', 'organization'], order_by="full_name")
+	context.speakers = frappe.get_list("Conference Speakers", filters={'published': 1}, fields=['photo', 'full_name', 'designation', 'organization'], order_by="full_name", ignore_permissions=True)

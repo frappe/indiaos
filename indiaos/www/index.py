1cb0c6fb0547353450607ad73ff8bbc0cf071658
Shivam Mishra <scm.mymail@gmail.com>
2019-11-19 10:45:18 +0530

    fix: no cache for index

--- indiaos/www/index.py
+++ indiaos/www/index.py
@@ -1,5 +1,7 @@
 import frappe
 
+no_cache = 1
+
 def get_context(context):
 	speakers = frappe.get_list("Conference Speaker", fields=['name', 'photo', 'full_name', 'designation', 'organization', 'published', 'route'], order_by="full_name", ignore_permissions=True)
 	context.talks = frappe.get_list("Conference Talk", filters={'show_on_home_page': 1},  fields=['*'], ignore_permissions=True)

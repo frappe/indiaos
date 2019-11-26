caea9ee3c5d0f322a80cbd42ad73944d0f6fe905
Shivam Mishra <scm.mymail@gmail.com>
2019-11-26 10:10:59 +0530

    feat: order talks

--- indiaos/www/index.py
+++ indiaos/www/index.py
@@ -4,5 +4,5 @@ no_cache = 1
 
 def get_context(context):
 	speakers = frappe.get_list("Conference Speaker", fields=['name', 'photo', 'full_name', 'designation', 'organization', 'published', 'route'], order_by="full_name", ignore_permissions=True)
-	context.talks = frappe.get_list("Conference Talk", filters={'show_on_home_page': 1, 'published': 1},  fields=['*'], ignore_permissions=True)
+	context.talks = frappe.get_list("Conference Talk", filters={'show_on_home_page': 1, 'published': 1},  fields=['*'], ignore_permissions=True, order_by="talk_title")
 	context.speakers = { speaker.name: speaker for speaker in speakers }

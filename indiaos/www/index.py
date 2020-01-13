6879a3bea860a1ca0600a4b2241b2d58a3e1e95f
Shivam Mishra <scm.mymail@gmail.com>
2020-01-13 16:03:44 +0530

    feat: disable ticketing from desk

--- indiaos/www/index.py
+++ indiaos/www/index.py
@@ -6,3 +6,4 @@ def get_context(context):
 	speakers = frappe.get_list("Conference Speaker", fields=['name', 'photo', 'full_name', 'designation', 'organization', 'published', 'route'], order_by="full_name", ignore_permissions=True)
 	context.talks = frappe.get_list("Conference Talk", filters={'show_on_home_page': 1, 'published': 1},  fields=['*'], ignore_permissions=True, order_by="talk_title")
 	context.speakers = { speaker.name: speaker for speaker in speakers }
+	context.settings = frappe.get_doc("Conference Settings")
